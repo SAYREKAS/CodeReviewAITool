c9ab3b22d588be74b18e58b0f1977f8ecaed90f1
sayrekas <chaotichoice@gmail.com>
2024-10-21 21:33:22 +0300

    Додано response_model для маршруту /files, щоб чітко вказати структуру відповіді за допомогою RepoFilesResponse. Вказано модель для обробки помилок ErrorResponse для маршруту /files. Додано AnalysisReport як модель для відповіді на маршруті /reviev, що полегшує обробку результатів аналізу. Змінено імпорт для запуску FastAPI з main:app на src.api.app:app, щоб відображати нову структуру папок.

--- src/api/app.py
+++ src/api/app.py
@@ -1,26 +1,40 @@
 from uvicorn import run
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from src.services.data_structures import CandidateLevel
+from src.services.data_structures import CandidateLevel, RepoFilesResponse, ErrorResponse, AnalysisReport
 from src.services.github_fetcher import get_repository_files
 from src.services.gpt_code_analyzer import GPTCandidateAnalyzer
 
 app = FastAPI()
 
 
-@app.post("/review")
-def review(assignment_description: str, github_repo_url: str, candidate_level: CandidateLevel):
-    files = get_repository_files(github_repo_url)
-    if files is None:
-        return {"message": "No repository files found"}
+@app.post(
+    path="/files",
+    tags=["GitHub"],
+    response_model=RepoFilesResponse,
+    responses={404: {"model": ErrorResponse}},
+)
+def fetch_files_from_the_specified_repository(github_repo_url: str):
+    return get_repository_files(github_repo_url)
 
-    analyzer = GPTCandidateAnalyzer(
-        file_contents=files,
+
+@app.post(
+    path="/review",
+    tags=["Code review AI tool"],
+    response_model=AnalysisReport,
+)
+def review(assignment_description: str, github_repo_url: str, candidate_level: CandidateLevel) -> AnalysisReport:
+    file_contents = get_repository_files(github_repo_url)
+    if not file_contents:
+        raise HTTPException(status_code=404, detail="No repository files found")
+
+    result = GPTCandidateAnalyzer(
+        file_contents=file_contents,
         candidate_level=candidate_level,
         assignment_description=assignment_description
     )
-    return analyzer.analysis_report
+    return result.analysis_report
 
 
 if __name__ == '__main__':
-    run(app="main:app", reload=True)
+    run(app="src.api.app:app", reload=True)
