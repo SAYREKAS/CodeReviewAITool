a0cb4cc2f185510fb8f9c34d1339945a65d455ea
sayrekas <chaotichoice@gmail.com>
2024-10-21 21:40:32 +0300

    Типізація, оптимізація, обробка виключень та помилок

--- src/services/github_fetcher.py
+++ src/services/github_fetcher.py
@@ -1,11 +1,11 @@
 import os
 
-from github import Auth
-from github import Github
+from fastapi import HTTPException
+from github import Auth, Github
 from loguru import logger
 from dotenv import load_dotenv
 
-from src.services.data_structures import Content
+from src.services.data_structures import RepoFilesResponse, Content
 
 load_dotenv()
 
@@ -13,7 +13,7 @@ auth = Auth.Token(os.getenv('GITHUB_ACCESS_TOKEN'))
 g = Github(auth=auth)
 
 
-def get_repository_files(repo_url: str) -> list[Content] | None:
+def get_repository_files(repo_url: str) -> RepoFilesResponse:
     try:
         logger.info(f"Fetching files from repository: {repo_url}")
 
@@ -24,7 +24,6 @@ def get_repository_files(repo_url: str) -> list[Content] | None:
 
         owner = parts[3]
         repo_name = parts[4]
-
         logger.debug(f"Owner: {owner}, Repository: {repo_name}")
 
         repo = g.get_repo(f"{owner}/{repo_name}")
@@ -42,16 +41,20 @@ def get_repository_files(repo_url: str) -> list[Content] | None:
                         logger.debug(f"Fetching file: {content.path}")
                         decoded_content = content.decoded_content.decode('utf-8')
                         file_contents.append(Content(filename=content.path, file_content=decoded_content))
-                    except Exception as e:
-                        logger.error(f"Error decoding file {content.path}: {e}")
+                    except Exception as ex:
+                        logger.error(f"Error decoding file {content.path}: {ex}")
 
         # Get contents of the root directory
         root_contents = repo.get_contents("")
         get_files_in_directory(root_contents)
 
         logger.info(f"Retrieved {len(file_contents)} files from repository {repo_name}")
-        return file_contents
+        return RepoFilesResponse(files=file_contents, count=len(file_contents))
 
-    except Exception as e:
-        logger.error(f"Error: {e}")
-        return None
+    except ValueError as ve:
+        logger.error(f"Validation error: {ve}")
+        raise HTTPException(status_code=400, detail=str(ve))
+
+    except Exception as ex:
+        logger.error(f"Error: {ex}")
+        raise HTTPException(status_code=404, detail="Failed to fetch repository files")
